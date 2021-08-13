0c80372585cfe56a2cb573d7001f4cacbd192634
chungjung <chungjung.d@gmail.com>
2021-08-13 20:21:39 +0900

    jeonbux -> jeonbuk

--- Frontend/navy-bottle-fe/src/pages/JeonbukCovid.js
+++ Frontend/navy-bottle-fe/src/pages/JeonbukCovid.js
@@ -2,14 +2,14 @@ import React from 'react';
 import '../styles/JeonbukCovid.scss'
 import { FixWrapper , FixMapIMGWrapper} from '../components/fixComponent';
 import MarkerRenderComponet from '../components/MarkerRenderComponet';
-import jeonbux_img from '../img/background/Jeonbuk_covid_img.jpg';
+import jeonbuk_img from '../img/background/Jeonbuk_covid_img.jpg';
 
 const JeonbukCovid = () => {
   return (
     <div className="jeonbuk-covid-body-wrapper">
       <FixWrapper>
         <FixMapIMGWrapper>
-          <img src ={jeonbux_img} alt="jeonbux_map_img" className="jeonbux_img"/>
+          <img src ={jeonbuk_img} alt="jeonbux_map_img" className="jeonbuk_img"/>
         </FixMapIMGWrapper>
       </FixWrapper>
 
