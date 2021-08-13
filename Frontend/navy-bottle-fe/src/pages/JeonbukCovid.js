a1ffd585802899894d070a8cede5b9633f591e16
junyoung yoon <junyoung27@gm.gist.ac.kr>
2021-08-13 19:59:58 +0900

    jeonbuk jeonnam marker

--- Frontend/navy-bottle-fe/src/pages/JeonbukCovid.js
+++ Frontend/navy-bottle-fe/src/pages/JeonbukCovid.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/JeonbukCovid.scss'
 import { FixWrapper , FixMapIMGWrapper} from '../components/fixComponent';
+import MarkerRenderComponet from '../components/MarkerRenderComponet';
 import jeonbux_img from '../img/background/Jeonbuk_covid_img.jpg';
 
 const JeonbukCovid = () => {
@@ -11,6 +12,10 @@ const JeonbukCovid = () => {
           <img src ={jeonbux_img} alt="jeonbux_map_img" className="jeonbux_img"/>
         </FixMapIMGWrapper>
       </FixWrapper>
+
+      <MarkerRenderComponet top={60} left={56.57} color='green'/>
+      <MarkerRenderComponet top={50.27} left={43.13} color='yellow'/>
+      <MarkerRenderComponet top={37} left={81.3} color='red'/>
 
       <div className="jeonnam-covid-body">
 
--- Frontend/navy-bottle-fe/src/pages/JeonnamCovid.js
+++ Frontend/navy-bottle-fe/src/pages/JeonnamCovid.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/JeonnamCovid.scss'
 import { FixWrapper , FixMapIMGWrapper} from '../components/fixComponent';
 import jeonnam_img from '../img/background/Jeonnam_covid_img.jpg';
+import MarkerRenderComponet from '../components/MarkerRenderComponet';
 
 const JeonnamCovid = () => {
   return (
@@ -11,6 +12,12 @@ const JeonnamCovid = () => {
           <img src ={jeonnam_img} alt="jeonnam_map_img" className="jeonnam_img"/>
         </FixMapIMGWrapper>
       </FixWrapper>
+
+      <MarkerRenderComponet top={37.9} left={58.4} color='green'/>
+      <MarkerRenderComponet top={31.5} left={62.75} color='green'/>
+      <MarkerRenderComponet top={26.2} left={58.7} color='green'/>
+      <MarkerRenderComponet top={20.38} left={42.25} color='yellow'/>
+      <MarkerRenderComponet top={65} left={72.74} color='red'/>
 
       <div className="jeonnam-covid-body">
 
