c517ec8f38d327da58a32fdfcb094ee24e0ce058
chungjung <chungjung.d@gmail.com>
2021-08-16 07:01:32 +0900

    FE-57

--- Frontend/navy-bottle-fe/src/components/CovidHospitalList.js
+++ Frontend/navy-bottle-fe/src/components/CovidHospitalList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import HospitalInfo from './logic/HospitalInfo';
 import { BasicData  ,HospitalCurrentDataDict} from './Data/Data';
+import { sorted } from './logic/HospitalSort';
 
 const FixWrapper = styled.div`
   position: absolute;
@@ -227,24 +228,7 @@ const CovidHospitalList = ({ zoom_in }) => {
   }, [buttonClick]);
 
 
-  const clonedeep = require('lodash.clonedeep');
-  const HospitalList = clonedeep(HospitalCurrentDataDict);
 
-  const sorted = Object.keys(HospitalList).sort(function(a,b){
-
-    const a_hospital = HospitalList[a];
-    const b_hospital = HospitalList[b];
-    const a_serious_unuse = a_hospital[1];
-    const a_mild_unuse = a_hospital[3];
-    const a_man = a_hospital[4];
-    const a_confusion_rate = (100 * (a_serious_unuse + a_mild_unuse)) / a_man;
-
-    const b_serious_unuse = b_hospital[1];
-    const b_mild_unuse = b_hospital[3];
-    const b_man = b_hospital[4];
-    const b_confusion_rate = (100 * (b_serious_unuse + b_mild_unuse)) / b_man;
-    return a_confusion_rate-b_confusion_rate
-  })
 
   
 
