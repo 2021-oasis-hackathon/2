f341229124ebcaa619dd4d51833233f10c9972a3
chungjung <chungjung.d@gmail.com>
2021-08-17 01:25:34 +0900

    de

--- Backend/navybottle_be/covid_data/views.py
+++ Backend/navybottle_be/covid_data/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+ 
+class UserView(APIView):
+    def get(self, request):
+        return Response("ok", status=200)#테스트용 Response
